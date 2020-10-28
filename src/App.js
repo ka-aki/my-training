f442abbe0e949c94aa809307ae51b2b7cd042697
Aki Kahamura <aki.kahamura@sharefull.com>
2020-10-28 18:32:03 +0900

    add: squares

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tbody>
+          {[...Array(10)].map((v, i) => (
+            <Row key={i} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
+
+const Row = () => {
+  return (
+    <tr>
+      {[...Array(10)].map((v, i) => (
+        <td key={i} />
+      ))}
+    </tr>
+  );
+};
 
 export default App;
